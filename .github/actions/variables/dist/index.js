905795e3dd56ec95ca8480c271dbcb0c193f7171
Saiteja8848 <saiteja8848>
2021-08-30 08:45:08 +0530

    update

--- .github/actions/variables/dist/index.js
+++ .github/actions/variables/dist/index.js
@@ -59,6 +59,13 @@ try{
    const time = new Date();
    core.setOutput("time",time.toTimeString());
 
+   const variables = {
+      "BUILDID" : "WUGLibs",
+      "PRODUCTVERSION": "66.0.0",
+      "CODELINE":"Main"
+   }
+   core.setOutput("variables",variables);
+   
    console.log(JSON.stringify(github,null,"\t"));
 }catch(error){
     core.setFailed(error.message)
